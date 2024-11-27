411b9e2f703bc8f186afa9b5aa02eef688c4238d
user <kkjjj16@gmail.com>
2024-11-27 16:11:48 +0900

    test2 추가 재미따!

--- src/main/java/com/git/controller/GitController.java
+++ src/main/java/com/git/controller/GitController.java
@@ -3,5 +3,10 @@ package com.git.controller;
 public class GitController {
 	public void test() {
 		System.out.println("안녕");
+		String test="123";
+	}
+	
+	public String test2() {
+		return "재미따!🙃";
 	}
 }
